8128d96b0f355254e6d3420fa35a9c5e24551617
Ricardo Alencar <ricfilho0007@gmail.com>
2023-08-23 13:55:02 -0300

    comments

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	//importação do módulo http
 	"net/http"
 )
 
+// define a função handler
+// w permite escrever a resposta HTTP para o cliente
+// r é um ponteiro para uma estrutura que contém informações sobre a requisição HTTP
 func handler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/index.html")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	//especifica qual porta o servidor irá rodar
 	http.ListenAndServe(":8080", nil)
 }
