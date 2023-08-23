cab15544ab4a370932963f2169b8434cf53d28cd
Ricardo Alencar <ricfilho0007@gmail.com>
2023-08-23 14:29:01 -0300

    criando objeto json

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	//importação do módulo http
+	"encoding/json"
 	"net/http"
 )
 
@@ -12,8 +13,22 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/index.html")
 }
 
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Pages  int    `json:"pages"`
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+
+	book := Book{Title: "Nome do livro", Author: "Autor do livro", Pages: 304}
+	json.NewEncoder(w).Encode(book)
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/book", GetBook)
 	//especifica qual porta o servidor irá rodar
 	http.ListenAndServe(":8080", nil)
 }
